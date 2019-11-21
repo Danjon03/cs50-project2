c21a45aed78349fa74bacf8129912d8515a9836d
Danjon03 <50589964+Danjon03@users.noreply.github.com>
2019-11-21 14:53:27 -0600

    Add files via upload

--- Texting Software/app.py
+++ Texting Software/app.py
@@ -1,47 +1,78 @@
 import os
-from flask import Flask, render_template, jsonify, request, session
+from flask import Flask, render_template, jsonify, request, session, Response, make_response
 from flask_socketio import SocketIO, emit
-
+from threading import Lock
+##from gevent import monkey
+##monkey.patch_all()
+async_mode = None
+from collections import OrderedDict
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "something super secret"
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+newListOfTextsInChats = {}
+listOfChats = []
+
+
 
 
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return render_template('index.html',async_mode=socketio.async_mode)
 
 @app.route("/channels", methods=["POST"])
 def channels():
 	name = request.form.get("name")
-	session["user"] = name
-	return render_template("createChatRoom.html", name=name, joinChatName="Skyrim Chat")
 	
-@app.route("/createChat", methods=["POST"])
-def createChat():
+	return render_template("createChatRoom.html", name=name, joinChatName="Skyrim Chat", listOfChats=listOfChats)
+	
+@app.route("/createChat/<name>", methods=["POST"])
+def createChat(name):
 	chatName = request.form.get("chatName")
-	user=session["user"]
-	return render_template("chatRoom.html", user=user, chatName=chatName)
+	listOfChats.append(chatName)
 	
-@app.route("/joinExistingChat", methods=["POST"])
-def joinExistingChat():
+	list = []
 	
+	newListOfTextsInChats.update({chatName: list})
+	print(newListOfTextsInChats)
 	
-	
-	user=session["user"]
-	return render_template("chatRoom.html", user=user, chatName="Skyrim Chat" )
-	
-	
+	return render_template("chatRoom.html", chatName=chatName, name=name, listOfTextsInChats=list)
 
+
+
+
+@app.route("/joinExistingChat/<name>/<chatID>")
+def joinExistingChat(name, chatID):
 	
+	print(name, chatID)
 	
+	listOfTextsInChats=newListOfTextsInChats.get(chatID)
 	
+	return render_template('chatRoom.html', name=name, chatName=chatID, listOfTextsInChats=listOfTextsInChats)
+
+@socketio.on('announce', namespace='/test')
+def text(message):
+	print("we have made it to announce")
+	
+	theList = newListOfTextsInChats.get(message['chat'])
+	theList.append(message['text'])
+	
+	newListOfTextsInChats[message['chat']] = theList
+	
+	print(newListOfTextsInChats)
+	emit("announce text", {"text": message['text'] }, broadcast=True)
+
+
 
 if __name__ == '__main__':
+	app.run(debug=True)
 	socketio.run(app)
-	index()
-	
+
+	##index()
+
 	"""
 	
 	flask run --no-reload 
